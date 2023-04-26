ee0ecde8b65f7f4ddb4cfbc8842e2e35ec076829
Hnaoyan <k022g00421@g.neec.ac.jp>
2023-04-26 15:43:53 +0900

    改行

--- Matrix.h
+++ Matrix.h
@@ -2,21 +2,20 @@
 #include<Vector3.h>
 #include<Matrix4x4.h>
 
-	Matrix4x4
-	    MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
-	//Matrix4x4 Add(const Matrix4x4& m1, const Matrix4x4& m2);
-	Matrix4x4 Subtract(const Matrix4x4& m1, const Matrix4x4& m2);
-	// Rotate
-	Matrix4x4 MakeRotateXMatrix(float radian);
-	Matrix4x4 MakeRotateYMatrix(float radian);
-	Matrix4x4 MakeRotateZMatrix(float radian);
-	Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
-	Matrix4x4 MakeInverse(const Matrix4x4& m);
-	Matrix4x4 MakeIdentity4x4();
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
+//Matrix4x4 Add(const Matrix4x4& m1, const Matrix4x4& m2);
+Matrix4x4 Subtract(const Matrix4x4& m1, const Matrix4x4& m2);
+// Rotate
+Matrix4x4 MakeRotateXMatrix(float radian);
+Matrix4x4 MakeRotateYMatrix(float radian);
+Matrix4x4 MakeRotateZMatrix(float radian);
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
+Matrix4x4 MakeInverse(const Matrix4x4& m);
+Matrix4x4 MakeIdentity4x4();
 
-	// Transpose
-	//Matrix4x4 Transpose(const Matrix4x4& m);
-	// Scale
-	Matrix4x4 MakeScaleMatrix(const Vector3& translate);
-	// Translate
-    Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
+// Transpose
+//Matrix4x4 Transpose(const Matrix4x4& m);
+// Scale
+Matrix4x4 MakeScaleMatrix(const Vector3& translate);
+// Translate
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
